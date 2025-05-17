60233f78dc88356f123ad0bd3a26bfa12e1f1d91
digitalk14 <vasilievdmtr@gmail.com>
2025-05-17 13:16:14 -0700

    docs(page): add description for the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,12 @@ import { Input, Credentials, CommitLogs } from "@/components";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-start sm:items-center justify-items-center bg-white dark:bg-gray-950 min-h-screen p-8  gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] transition-colors duration-200">
-      <header className="row-start-1 flex gap-[24px] flex-wrap items-end justify-between w-full max-w-[640px] mx-auto">
-        <h1 className="text-2xl font-bold text-left dark:text-[#666]">Git Commit<br/>Message Generator</h1>
+    <div className="flex flex-col items-start sm:items-center justify-items-center bg-white dark:bg-gray-950 min-h-screen p-8  gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] transition-colors duration-200">
+      <header className="row-start-1 flex gap-[16px] flex-wrap items-end justify-between w-full max-w-[640px] mx-auto">
+        <h1 className="text-xl sm:text-2xl font-bold text-left dark:text-[#666]">Git Commit<br/>Message Generator</h1>
+        <p>
+        Good Commit helps you stop worrying about writing perfect Git commit messages. Just type what you did and it generates a clean, conventional commit message for you, like: <span><i>"updated login button style"</i></span>
+        </p>
         <Credentials />
       </header>
       <main className="flex flex-col gap-[32px] w-full row-start-2 items-center " >
